deab084701eab533891504137355f24535dd4d05
naeem <naeem.tahir2810@gmail.com>
2023-09-13 01:03:29 +0500

    showing the comments dynamically with dummyComment data

--- assessmentApp/src/components/Comment.jsx
+++ assessmentApp/src/components/Comment.jsx
@@ -1,23 +1,23 @@
-import user1 from "../../../images/user1.png";
 import Like from "../../../images/like.svg";
+import UnLike from "../../../images/unlike.svg";
 
-const Comment = () => {
+// eslint-disable-next-line react/prop-types
+const Comment = ({name,text,likes,image} ) => {
   return (
     <div className="bg-white p-2 mb-3 ">
       <div className="flex items-center">
-        <img className="mr-2 mb-12" src={user1} alt="user1" />
+        <img className="mr-2 mb-12" src={image} alt="user1" />
 
         <div>
-          <h3 className="text-black flex-grow mt-2 ">Maria</h3>
+          <h3 className="text-black flex-grow mt-2 ">{name}</h3>
           <p className="text-grayText">
-            I was very glad to have you after such a long time. Can you plan a
-            meetup? Maybe this weekend?
+            {text}
           </p>
 
           <div className="flex mt-3 mb-2">
-            <img src={Like} />
-            <span className="ml-2  text-grayText">1</span>
-            <b className="ml-4 font-extrabold text-grayText">·</b>
+            <img src={likes > 0 ? Like : UnLike} />
+            <span className="ml-2  text-grayText">{likes}</span>
+            <span className="ml-4 font-extrabold text-grayText">·</span>
             <button className="ml-5 text-blue">Reply</button>
           </div>
         </div>
