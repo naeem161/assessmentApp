e228a8f4c58cd1e04f72fca0987edae3190f2cea
naeem <naeem.tahir2810@gmail.com>
2023-09-13 14:20:03 +0500

    removing un-nessary style

--- assessmentApp/src/App.jsx
+++ assessmentApp/src/App.jsx
@@ -61,11 +61,9 @@ function App() {
   };
 
   return (
-    // h-screen
-    <div className="bg-bodyBackground   flex justify-center  ">
+    <div className="bg-bodyBackground flex justify-center  ">
       <div
         className="container sm:w-3/5 md:w-3/5 lg:w-3/5 h-auto mt-6 "
-        style={{ border: "2px solid red" }}
       >
         {/* <div className="container h-2/4 w-2/4 " style={{border:'2px solid red'}} > */}
         {commentsData.map((comment) => (
